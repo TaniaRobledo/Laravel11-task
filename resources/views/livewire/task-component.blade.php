f0c746af0e50cc9e02c42d9142409fb8cf620a1d
TaniaRobledo <tania.robledoabenojar@riberadeltajo.es>
2025-01-28 13:54:40 +0100

    Creación del Modal

--- resources/views/livewire/task-component.blade.php
+++ resources/views/livewire/task-component.blade.php
@@ -1,3 +1,44 @@
-<div>
-   <h1>Bienvenido al gesto de tareas</h1>
+<div class="overflow-x-auto">
+   <table class="table-auto w-full border-collapse border border-gray-300">
+
+   <!-- Tabla -->
+    <thead>
+        <tr class="bg-purple-600 text-white">
+            <th class="border border-gray-300 px-4 py-2">Titulo</th>
+            <th class="border border-gray-300 px-4 py-2">Descripición</th>
+            <th class="border border-gray-300 px-4 py-2">Acciones</th>
+        </tr>
+    </thead>
+
+    <tbody>
+        <tr class="bg-purple-50 hover:bg-purple-100">
+            <td  class="border border-gray-300 px-4 py-2">Tarea 1</td>
+            <td  class="border border-gray-300 px-4 py-2">Descripción de la tarea 1</td>
+            <td  class="border border-gray-300 px-4 py-2 flex gap-2 justify-center">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Editar
+                </button>
+                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    Eliminar
+                </button>
+        </tr>
+
+        <tr class="bg-purple-50 hover:bg-purple-100">
+            <td  class="border border-gray-300 px-4 py-2">Tarea 2</td>
+            <td  class="border border-gray-300 px-4 py-2">Descripción de la tarea 2</td>
+            <td  class="border border-gray-300 px-4 py-2 flex gap-2 justify-center">
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Editar
+                </button>
+                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                    Eliminar
+                </button>
+        </tr>
+    </tbody>
+
+    <!-- Tabla -->
+   </table>
+    
+               
+   
 </div>
