157f8cfe1c6bcd400baebd8ebeb697cf466d981d
TaniaRobledo <tania.robledoabenojar@riberadeltajo.es>
2025-01-30 13:57:20 +0100

    Función crear tarea añadida

--- app/Livewire/TaskComponent.php
+++ app/Livewire/TaskComponent.php
@@ -3,21 +3,69 @@
 namespace App\Livewire;
 
 use Livewire\Component;
+use App\Models\Task;
+use Illuminate\Support\Facades\Auth;
 
 
 class TaskComponent extends Component
 {
+    public $tasks = [];
+    public $title;
+    public $description;
     public $modal = false;
+
+    public function mount(){
+        $this->tasks = $this->getTasks();
+    }
+
+   
+    public function getTask(){
+      return Task::where('user_id', Auth::user()->id)->get();
+    }
+   
     public function openCreateModal()
     {
         $this->clearFields();
         $this->modal = true;
     }
 
+    private function clearFields()
+    {
+        // Clear fields logic here
+    }
+
     public function closeCreateModal()
     {
-        $this->clearFields();
+       
         $this->modal = false;
     }
+
+    public function createTask()
+    {
+
+        $this->validate([
+            'title' => 'required|string|max:225',
+            'description' => 'required|string|max:1000',
+        ]);
+
+        $task = new Task();
+        $task->title = $this->title;
+        $task->description = $this->description;
+        $task->user_id = Auth::user()->id;
+        $task->save();
+        $this->clearFields();
+        $this->closeCreateModal();
+        $this->tasks = $this->getTasks();
+      
+    }
+
+    public function updateTask(Task $task)
+    {
+        $this->title = $task->title;
+        $this->description = $task->description;
+        $this->modal = true;
+       
+    }
+    
 }
 
--- resources/views/livewire/task-component.blade.php
+++ resources/views/livewire/task-component.blade.php
@@ -1,76 +1,80 @@
+
 <div class="overflow-x-auto">
-    <button class="bg-purple-600 text-white px-4 py-2 rounden hover:bg-purple-700 my-6"
-    wire:click="openCreateModal">Crear tarea</button>
-        
+    <button class="bg-purple-600 text-white px-4 py-2 rounded hover:bg-purple-700 my-6" wire:click="openCreateModal">
+       Nuevo
     </button>
-   <table class="table-auto w-full border-collapse border border-gray-300">
 
-   <!-- Tabla -->
-    <thead>
-        <tr class="bg-purple-600 text-white">
-            <th class="border border-gray-300 px-4 py-2">Titulo</th>
-            <th class="border border-gray-300 px-4 py-2">Descripición</th>
-            <th class="border border-gray-300 px-4 py-2">Acciones</th>
-        </tr>
-    </thead>
+    <table class="table-auto w-full border-collapse border border-gray-300">
+        <!-- Tabla -->
+        <thead>
+            <tr class="bg-purple-600 text-white">
+                <th class="border border-gray-300 px-4 py-2">Titulo</th>
+                <th class="border border-gray-300 px-4 py-2">Descripción</th>
+                <th class="border border-gray-300 px-4 py-2">Acciones</th>
+            </tr>
+        </thead>
 
-    <tbody>
-        <tr class="bg-purple-50 hover:bg-purple-100">
-            <td  class="border border-gray-300 px-4 py-2">Tarea 1</td>
-            <td  class="border border-gray-300 px-4 py-2">Descripción de la tarea 1</td>
-            <td  class="border border-gray-300 px-4 py-2 flex gap-2 justify-center">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Editar
-                </button>
-                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                    Eliminar
-                </button>
-        </tr>
+        <tbody>
+            <tr class="bg-purple-50 hover:bg-purple-100">
+                <td class="border border-gray-300 px-4 py-2">Tarea 1</td>
+                <td class="border border-gray-300 px-4 py-2">Descripción de la tarea 1</td>
+                <td class="border border-gray-300 px-4 py-2 flex gap-2 justify-center">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    wire:click="udpateTask({{ $task-> }})">
+                        Editar
+                    </button>
+                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                        Eliminar
+                    </button>
+                </td>
+            </tr>
 
-        <tr class="bg-purple-50 hover:bg-purple-100">
-            <td  class="border border-gray-300 px-4 py-2">Tarea 2</td>
-            <td  class="border border-gray-300 px-4 py-2">Descripción de la tarea 2</td>
-            <td  class="border border-gray-300 px-4 py-2 flex gap-2 justify-center">
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Editar
-                </button>
-                <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
-                    Eliminar
-                </button>
-        </tr>
-    </tbody>
+            @foreach($tasks as $task)
+            <tr class="bg-purple-50 hover:bg-purple-100">
+                <td class="border border-gray-300 px-4 py-2">Tarea 2</td>
+                <td class="border border-gray-300 px-4 py-2">Descripción de la tarea 2</td>
+                <td class="border border-gray-300 px-4 py-2 flex gap-2 justify-center">
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    wire:click="udpateTask({{ $task-> }})">
+                        Editar
+                    </button>
+                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                        Eliminar
+                    </button>
+                </td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
 
-    <!-- Tabla -->
-   </table>
+    <!-- Modal Background -->
+    @if($modal)
+    <div class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+        <!-- Modal Content -->
+        <div class="bg-white p-6 rounded-lg w-96">
+            <!-- Modal Title -->
+            <h2 class="text-xl font-bold text-blue-600 mb-4">Crear una nueva tarea</h2>
 
-   <!-- Modal Background -->
-<div class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
-    <!-- Modal Content -->
-    <div class="bg-white p-6 rounded-lg w-96">
-        <!-- Modal Title -->
-        <h2 class="text-xl font-bold text-blue-600 mb-4">Crear una nueva tarea</h2>
+            <!-- Form -->
+            <form wire:submit.prevent="createTask">
+                <!-- Titulo Input -->
+                <label for="titulo" class="block text-sm font-medium text-gray-700">Título</label>
+                <input id="titulo" wire:model="titulo" type="text" class="mt-2 w-full p-2 border border-gray-300 rounded-md" placeholder="Escribe el título de la tarea" required>
 
-        <!-- Form -->
-        <form>
-            <!-- Titulo Input -->
-            <label for="titulo" class="block text-sm font-medium text-gray-700">Título</label>
-            <input id="titulo" type="text" class="mt-2 w-full p-2 border border-gray-300 rounded-md" placeholder="Escribe el título de la tarea" required>
+                <!-- Descripción Input -->
+                <label for="descripcion" class="block text-sm font-medium text-gray-700 mt-4">Descripción</label>
+                <input id="descripcion" wire:model="descripcion" type="text" class="mt-2 w-full p-2 border border-gray-300 rounded-md" placeholder="Escribe una descripción" required>
 
-            <!-- Descripción Input -->
-            <label for="descripcion" class="block text-sm font-medium text-gray-700 mt-4">Descripción</label>
-            <input id="descripcion" type="text" class="mt-2 w-full p-2 border border-gray-300 rounded-md" placeholder="Escribe una descripción" required>
-
-            <!-- Buttons -->
-            <div class="flex justify-end mt-6">
-                <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded-md mr-2">Crear tarea</button>
-                <button type="button" class="px-4 py-2 bg-gray-300 text-gray-700 rounded-md">Cancelar</button>
-            </div>
-        </form>
-    </div>
-</div>
-
-
-    
+                <!-- Buttons -->
+        
                
-   
+                <div class="flex justify-end mt-6">
+                    <button type="submit" class="px-4 py-2 bg-blue-600 text-white rounded-md mr-2" wire:click.prevent="createTask">Crear tarea</button>
+                    <button type="button" class="px-4 py-2 bg-gray-300 text-gray-700 rounded-md" wire:click="closeCreateModal">Cancelar</button>
+                </div>
+              
+            </form>
+        </div>
+    </div>
+    @endif
 </div>
